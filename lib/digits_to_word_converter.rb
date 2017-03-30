5603f3ad9c87ab6af618ac51de727788c1c05630
ravindra-wwindia <ravindra.ghare@wwindia.com>
2017-03-30 19:11:57 +0530

    adds comments and re-factor code

--- lib/digits_to_word_converter.rb
+++ lib/digits_to_word_converter.rb
@@ -1,6 +1,7 @@
 require 'set'
 require 'benchmark'
 
+# From given a 10 digit number, it will return all possible words or combination of words from the dictionary file
 class DigitsToWordConverter
 
   NUMBER_TO_WORD_MAPPER = {
@@ -14,6 +15,7 @@ class DigitsToWordConverter
     '9' => %w(w x y z)
   }.freeze
 
+  # validate given number and process it
   def parse(number)
     if /\A[2-9]{10}\z/ =~ number
       possible_permutations = number_permutations(number)
@@ -23,31 +25,38 @@ class DigitsToWordConverter
     end
   end
 
+  # Find all possible combinations of number by spliting it
+  # into possible two and three slices of number
+  # Return type array
   def number_permutations(number)
     permutations = []
 
     [3, 4, 5, 6, 7, 10].each do |pattern|
       permutations << split_in_two(number, pattern)
     end
-    [[3,3],[3,4],[4,3]].each do |pattern|
+    [[3, 3], [3, 4], [4, 3]].each do |pattern|
       permutations << split_in_three(number, pattern.first, pattern.last)
     end
 
     permutations
   end
 
+  # possible two slices [5, 5], [3, 7], [7, 3], [4, 6], [6, 4], [10, 0]
   def split_in_two(number, length)
     [number.slice(0, length), number.slice(length, 10 - length)]
   end
 
+  # possible three slices [3, 3, 4], [4, 3, 3], [3, 4, 3]
   def split_in_three(number, length1, length2)
     [number.slice(0, length1), number.slice(length1, length2), number.slice(length1 + length2, 10)]
   end
 
+  # Fetch all dictionary words from dictionary.txt and load in Set
   def dictionary
     @dictionary ||= Set.new(File.read('../dictionary.txt').downcase.split("\n"))
   end
 
+  # Return final mapped words which are mached in dictionary.txt
   def process_combinations(combinations)
     output = []
     combinations.each do |combination|
@@ -58,22 +67,22 @@ class DigitsToWordConverter
     end
     mapped_words = []
     output.each { |f| mapped_words.concat(f[0].product(*f[1..-1])) }
-    p mapped_words
+    mapped_words.to_s
   end
 
+  # Return all combination of words
   def get_all_matched_words(combination, dictionary)
     all_matched_words = []
-    combination.map {|pattern| all_matched_words.push(find_matches(pattern, dictionary)) unless pattern.empty? }
+    combination.map { |pattern| all_matched_words.push(find_matches(pattern, dictionary)) unless pattern.empty? }
     all_matched_words
   end
 
+  # Process number combinations and find its possible words
   def find_matches(number, dictionary)
     words = []
     x = []
-    number.split('').each { |var| x.push(NUMBER_TO_WORD_MAPPER[var]) }
+    number.split('').each { |digit| x.push(NUMBER_TO_WORD_MAPPER[digit]) }
     x[0].product(*x[1..-1]).each { |val| words.push(val.join) if dictionary.include? val.join } unless x.empty?
     words
   end
 end
-
-p Benchmark.measure{ DigitsToWordConverter.new.parse('6686787825') }
