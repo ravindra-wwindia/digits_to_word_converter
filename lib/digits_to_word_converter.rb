f904952f2a7d61d4018aae9740dcd3022fe63ec2
ravindra-wwindia <ravindra.ghare@wwindia.com>
2017-03-30 13:37:00 +0530

    validate and generates possible combinations of given number

--- lib/digits_to_word_converter.rb
+++ lib/digits_to_word_converter.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class DigitsToWordConverter
 
   NUMBER_TO_WORD_MAPPER = {
@@ -11,4 +13,34 @@ class DigitsToWordConverter
     '9' => %w(w x y z)
   }.freeze
 
+  def parse(number)
+    if /\A[2-9]{10}\z/ =~ number
+      possible_permutations = number_permutations(number)
+    else
+      puts 'Mobile number should be only 10 digit as well as should not contain 0 and 1'
+    end
+  end
+
+  def number_permutations(number)
+    permutations = []
+
+    [3, 4, 5, 6, 7, 10].each do |pattern|
+      permutations << split_in_two(number, pattern)
+    end
+    [[3,3],[3,4],[4,3]].each do |pattern|
+      permutations << split_in_three(number, pattern.first, pattern.last)
+    end
+
+    permutations
+  end
+
+  def split_in_two(number, length)
+    [number.slice(0, length), number.slice(length, 10 - length)]
+  end
+
+  def split_in_three(number, length1, length2)
+    [number.slice(0, length1), number.slice(length1, length2), number.slice(length1 + length2, 10)]
+  end
 end
+
+DigitsToWordConverter.new.parse('6686787825')
