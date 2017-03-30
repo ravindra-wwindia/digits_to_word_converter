a1ae4388a7c6aec193721e4866fb433e5ee9a55e
ravindra-wwindia <ravindra.ghare@wwindia.com>
2017-03-30 12:01:22 +0530

    initial commit

---
+++ lib/digits_to_word_converter.rb
@@ -0,0 +1,14 @@
+class DigitsToWordConverter
+
+  NUMBER_TO_WORD_MAPPER = {
+    '2' => %w(a b c),
+    '3' => %w(d e f),
+    '4' => %w(g h i),
+    '5' => %w(j k l),
+    '6' => %w(m n o),
+    '7' => %w(p q r s),
+    '8' => %w(t u v),
+    '9' => %w(w x y z)
+  }.freeze
+
+end
